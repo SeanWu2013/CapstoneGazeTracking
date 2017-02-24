887974d391a959d93299b029a6cb2590a5e6232d
SeanWu2013 <sww35@scarletmail.rutgers.edu>
2017-02-23 20:23:18 -0500

    Initial Commit

---
+++ face_detect_cv3.py
@@ -0,0 +1,46 @@
+import cv2
+import sys
+
+# Initialize video feed
+cap = cv2.VideoCapture(0)
+
+# Get user supplied values
+# imagePath =
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    # cv2.imshow('frame', frame)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30)
+        # flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+    print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Faces found", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
