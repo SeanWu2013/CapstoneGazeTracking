b1ce96e00515d6451e480d4adcb73f567a217af7
SeanWu2013 <sww35@scarletmail.rutgers.edu>
2017-03-26 16:54:37 -0400

    New file for facial landmarking

---
+++ webcamFaceDetection.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image and
+#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
+#   points on the face such as the corners of the mouth, along the eyebrows, on
+#   the eyes, and so forth.
+#
+#   This face detector is made using the classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
+#   and sliding window detection scheme.  The pose estimator was created by
+#   using dlib's implementation of the paper:
+#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
+#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
+#   and was trained on the iBUG 300-W face landmark dataset.
+#
+#   Also, note that you can train your own models using dlib's machine learning
+#   tools. See train_shape_predictor.py to see an example.
+#
+#   You can get the shape_predictor_68_face_landmarks.dat file from:
+#   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#   or
+#       python setup.py install --yes USE_AVX_INSTRUCTIONS
+#   if you have a CPU that supports AVX instructions, since this makes some
+#   things run faster.  
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake and boost-python installed.  On Ubuntu, this can be done easily by
+#   running the command:
+#       sudo apt-get install libboost-python-dev cmake
+#
+#   Also note that this example requires scikit-image which can be installed
+#   via the command:
+#       pip install scikit-image
+#   Or downloaded from http://scikit-image.org/download.html. 
+
+import sys
+import os
+import dlib
+import glob
+
+from skimage import io
+sys.path.append('/usr/local/lib/python2.7/site-packages')
+import cv2
+import numpy
+
+predictor_path = sys.argv[1]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+win = dlib.image_window()
+win2 = dlib.image_window()
+
+cap = cv2.VideoCapture(0)
+count = 0;
+
+while(True):
+    ret, img = cap.read()
+    img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
+
+    win.clear_overlay()
+    win2.clear_overlay()
+    win.set_image(img)
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    if(count % 3 == 0):
+    	dets = detector(img, 1)
+    	count = 0
+    else:
+    	count = count + 1
+    print("Number of faces detected: {}".format(len(dets)))
+
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+                                                  shape.part(1)))
+        # Draw the face landmarks on the screen.
+        win.add_overlay(shape)
+        win2.add_overlay(shape)
+
+    win.add_overlay(dets)
+    win2.add_overlay(dets)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+    	break
+
+cap.release()
+cv2.destroyAllWindows()
